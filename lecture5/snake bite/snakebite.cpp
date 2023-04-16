a318259abad98d48ca422a4606f897dd4b33d6db
hanstldk <127701653+hanstldk@users.noreply.github.com>
2023-04-16 22:45:45 +0900

    6주차 스네이크 바이트 만들기

--- lecture5/snake bite/snakebite.cpp
+++ lecture5/snake bite/snakebite.cpp
@@ -2,6 +2,172 @@
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[200], tailY[200];
+int nTail;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+bool gameOver;
+int ranklist[10];
+
+char buffer[height][width];
+char screen[height][width];
+
+void gotoxy(int x, int y) {
+    //x, y 좌표 설정
+    COORD pos = { x,y };
+    //커서 이동
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void ranking() {
+    
+}
+
+void print_ranking() {
+    for (int i = 0;i < 10;i++) {
+        std::cout << to_string(i + 1) + "위" + " : " + to_string(ranklist[i])<<endl;
+    }
+   
+}
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+    nTail = 0;
+    tailX[100] = {};
+    tailY[100] = {};
+}
+
+void Draw() {
+    system("cls");
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else {
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    cout << "Score:" << score << endl; //점수를 출력합니다.
+    cout << "Length:" << nTail << endl; //뱀의 길이를 출력합니다.
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
+}
+
+void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir) {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+
+    //뱀이 벽에 부딪혔는지 체크합니다.
+    if (x > width || x < 0 || y > height || y < 0)
+        gameOver = true;
+
+    //뱀의 머리가 몸통과 부딪혔는지 체크합니다.
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+    }
+
+    //뱀이 과일을 먹었는지 체크합니다.
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+    if (gameOver) {
+        cout << "Game Over" << endl;
+        cout << "Score: " << score << endl;
+    }
+}
+
 
 // game_state == 0 일때
 int print_title_screen()
@@ -26,26 +192,6 @@ int print_title_screen()
     return 0;
 }
 
-void gotoxy(int x, int y) {
-    //x, y 좌표 설정
-    COORD pos = { x,y };
-    //커서 이동
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-}
-
-int print_game_screen(int stage_width, int stage_height)
-{
-    gotoxy(15, 15);
-    for (int i = 15;i <= stage_width + 14;i++) {
-        for (int j = 15;j <= stage_height + 14;j++) {
-            if (i == 15 || i == stage_width + 14 || j == 15 || j == stage_height + 14) {
-                gotoxy(i, j);
-                std::cout << "^^";
-            }
-        }
-    }
-    return 0;
-}
 
 // game_state == 2 일때
 int print_introduction_screen()
@@ -57,9 +203,11 @@ int print_introduction_screen()
 
 int main()
 {
-
-    int stage_width = 0;
-    int stage_height = 0;
+    CONSOLE_CURSOR_INFO cursorInfo;
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleCursorInfo(consoleHandle, &cursorInfo);
+    cursorInfo.bVisible = false; // 커서가 보이지 않도록 설정
+    SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 
     int game_state = 0;
     int is_game_running = 1;
@@ -81,6 +229,7 @@ int main()
                 game_state = 2;
                 break;
             case '3':
+                game_state = 3;
                 break;
             case '4':
                 is_game_running = 0;
@@ -93,12 +242,16 @@ int main()
             }
             break;
         case 1:
-            std::cout << "스테이지 가로 길이 입력:";
-            std::cin >> stage_width;
-            std::cout << "스테이지 세로 길이 입력:";
-            std::cin >> stage_height;
-            print_game_screen(stage_width,stage_height);
-            key_input = _getch();
+            Setup();
+            while (!gameOver) {
+                Draw();
+                Input();
+                Logic();
+                Sleep(10); //게임 속도를 조절합니다.
+            }
+            ranking();
+            game_state = 0;
+            break;
         case 2:
             print_introduction_screen();
             key_input = _getch();
@@ -113,7 +266,20 @@ int main()
                 break;
             }
             break;
-
+        case 3:
+            print_ranking();
+            key_input = _getch();
+            switch (key_input)
+            {
+            case 1:
+                game_state = 0;
+                break;
+            case 2:
+                break;
+            default:
+                break;
+            }
+            break;
         default:
             break;
         }
